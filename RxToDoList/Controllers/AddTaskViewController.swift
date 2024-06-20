f6f7d812ecfe713eb06d6f98e9a97e3665f3e4f6
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-06-20 23:04:37 +0900

    lay out user interface

---
+++ RxToDoList/Controllers/AddTaskViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AddTaskViewController.swift
+//  RxToDoList
+//
+//  Created by joe on 6/20/24.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+    
+}
---
+++ RxToDoList/Controllers/TaskListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TaskListViewController.swift
+//  RxToDoList
+//
+//  Created by joe on 6/19/24.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath)
+        return cell
+    }
+}
